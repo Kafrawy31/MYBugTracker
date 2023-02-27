9e997b94859eae78e650ac4a4e3f1b093f63d95a
Ahmed Elkafrawy <ahmedkhaledkafrawy31@gmail.com>
2023-02-27 09:19:41 +0000

    install packages and grab data from BackEnd

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,11 +1,24 @@
 import React from "react";
 import { useEffect, useState } from "react";
 import "./App.css";
+import Register from "./components/Register.js";
+import TicketList from "./components/TicketList.js";
+import axios from "axios";
+
 
 function App() {
+  const [tickeTable , setTicketTable] = useState([]);
+  console.log(tickeTable);
+  useEffect(() =>{
+    axios('http://127.0.0.1:8000/api/ticket-list/')
+    .then(response => setTicketTable(response.data))
+    .catch(error => console.error(error))
+  },[])
+
   return (
     <div>
-      <h1>hello</h1>
+      <Register />
+      <button></button>
     </div>
   );
 }
